e18adc5c4260c86c21e136bf8e9891987c9b5b0a
wusspuss <wuss.puss@ya.ru>
2023-09-07 07:03:57 +0400

    Add first (more-or-less) working example. media_receiver.py - use with main.html from media receiver example from libdatachannel. It tries to copy the original media receiver tit for tat as much as possible with  the limitations of the C API, however it only works every other time. No idea why, yet.

---
+++ media_receiver.py
@@ -0,0 +1,35 @@
+import socket
+import json
+from libdatachannel import libdatachannel
+
+libdatachannel.init_logger(libdatachannel.LogLevel.LOG_VERBOSE)
+
+def onGatheringStateChange(state, pc):
+    print("Gathering State:", state)
+    if state == libdatachannel.GatheringState.GATHERING_COMPLETE:
+        print(json.dumps({'sdp': pc.local_description, 'type': 'offer'}))
+
+def onMessage(message, size):
+    sock.sendto(message, ('127.0.0.1', 5000))
+    # print('.', end='', flush=True)
+
+pc = libdatachannel.PeerConnection()
+# with libdatachannel.PeerConnection() as pc:
+pc.gathering_state_change_callback = lambda state: onGatheringStateChange(state, pc)    
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+track = pc.add_track(b'\n'.join([b"video 52313 UDP/TLS/RTP/SAVPF 96",
+                                 b"a=mid:video",
+                                 b"a=recvonly",
+                                 b"b=AS:3000",
+                                 b"a=rtpmap:96 H264/90000",
+                                 b"a=rtcp-fb:96 nack",
+                                 b"a=rtcp-fb:96 nack pli",
+                                 b"a=rtcp-fb:96 goog-remb",
+                                 b"a=fmtp:96 profile-level-id=42e01f;packetization-mode=1;"
+                                 b"level-asymmetry-allowed=1",
+                                 ]))
+track.message_callback = onMessage
+pc.set_local_description()
+pc.remote_description=json.loads(input("Remote description:"))['sdp'].encode()
+input()
+
